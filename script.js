4bce33d69d84a10c834effedc67c6e9b1f128c2d
riAnriAn0 <156357069+riAnriAn0@users.noreply.github.com>
2024-03-19 17:31:58 -0300

    edit01

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let listaDePalavras = ['','GATO','CANETA','BOLA','COMPUTADOR','JORNAL','CARRO','CHAVE','CACHORRO','CELULAR'];
+let listaDePalavras = ['MERCADO','GATO','CANETA','BOLA','COMPUTADOR','JORNAL','CARRO','CHAVE','CACHORRO','CELULAR'];
 
 
 function verPalavra(){
+    receberLetra.focus()
     if(novaPalavra.type == 'text'){
         novaPalavra.setAttribute('type','password');
     } else if(novaPalavra.type == 'password') {
@@ -24,56 +25,52 @@ salvar.addEventListener('click',()=>{
     numeroDeLetras.textContent = novaPalavra.value.length;
     
     if (cont == 9) {
-        cont = 1;
+        cont = 0;
     } else {
         cont++;
     }
-
-        receberLetra.focus()
+    
+    receberLetra.focus()
 });
 
 let receberLetra  = document.getElementById('receberLetra')
 let enviarNovaletra = document.getElementById('enviarNovaletra')
 let checks = document.querySelectorAll('#check')
 let cont2 = 0
+let span = document.querySelector('#derrota')
 
-enviarNovaletra.addEventListener('click',()=>{
-    
+document.addEventListener('keyup',(event)=>{
+   if(event.key == 'Enter'){
+    sistemaDeJogo()
+   }
+})
+
+function sistemaDeJogo(){
+
     let arrayLetras = Array.from(novaPalavra.value)
     let letra = receberLetra.value.toUpperCase();
-    
+
     if( receberLetra.value == '' ){
         console.log('DIGETE UMA LETRA')
     } else{
-        arrayLetras.forEach((element,indice) => {       
-            
-            if (element === letra) {
-                qualquerCoisa = 1
-            } else if (element !== letra) {
-                qualquerCoisa = 2
-            }
 
-           
-            switch (qualquerCoisa){
-                case 1:
-                    inputs[indice].value = letra;
-                    break;
-                case 2:
-                    checks[cont2].classList.add('marcado');
-                    break;
-            };  
-            
-            
-        });
-        if (cont == 6) {
-            cont = 0;
-        } else {
-            cont++;
+        const indice = arrayLetras.indexOf(letra);
+
+        if (indice !== -1) {
+            inputs[indice].value = letra;
+        } else if(cont2 < 6 ){
+            checks[cont2].classList.add('marcado');
+            cont2 += 1
+        }
+
+        if(cont2 >= 6){
+            novaPalavra.setAttribute('type','text');
+            span.textContent = 'VC PERDEU DESEJA RECOMEÇAR'
+            receberLetra.readOnly = true;
+
         }
         receberLetra.value = ''
         receberLetra.focus()
-    }  
-})
-
-
+    };
+};
 
